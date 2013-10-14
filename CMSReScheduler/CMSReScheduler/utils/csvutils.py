98f83ade08d4e628dd68d24ba3110493ea32eb79
William Mak <william.mak@hotmail.ca>
2013-10-14 12:31:51 -0400

    Fixing a spelling mistake
    You'd think I could spell delimiter, but you would be wrong.

--- CMSReScheduler/CMSReScheduler/utils/csvutils.py
+++ CMSReScheduler/CMSReScheduler/utils/csvutils.py
@@ -15,7 +15,7 @@ def parse(filename, format, delimiter=','):
 	'''
 	items = []
 	with open(filename, 'rb') as csvfile:
-		parser = csv.reader(csvfile, delimeter)
+		parser = csv.reader(csvfile, delimiter)
 		for row in parser:
 			new_item = {}
 			for i in range(len(format)):
