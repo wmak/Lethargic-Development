7bef938791414c1a65f450895fa4966759968da1
William Mak <william.mak@hotmail.ca>
2013-10-19 20:40:08 -0400

    Update settings.py

--- CMSReScheduler/CMSReScheduler/settings.py
+++ CMSReScheduler/CMSReScheduler/settings.py
@@ -12,13 +12,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'CMSdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'pikachu',           # I'm sorry. Sorta. not really. Should change to a better password down the road.
+        'USER': 'djangouser',
+        'PASSWORD': 'bulbasaur',           # I'm sorry. Sorta. not really. Should change to a better password down the road.
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
