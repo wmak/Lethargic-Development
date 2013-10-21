e24e6da490f8f09ab8911c50b6cf05b9a193a7a7
Jeffrey Cheung <jeffrey.cheung1337@gmail.com>
2013-10-20 20:34:29 -0400

    Use os.path to get absolute path for static files

--- CMSReScheduler/CMSReScheduler/settings.py
+++ CMSReScheduler/CMSReScheduler/settings.py
@@ -73,9 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-
-    # Replace with absolute path to static folder of application on your local machine
-    '/Users/jeffreycheung/Lethargic-Development/CMSReScheduler/static/',
+    os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir, 'static')),
 )
 
 # List of finder classes that know how to find static files in
