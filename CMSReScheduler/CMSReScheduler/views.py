ed5977c647bc130c322a694f06cd393b39a42ea3
ShaiMitchell <shaimitchell@Shai-Mitchells-MacBook.local>
2013-10-23 03:42:06 -0400

    tried to generalize the csv importer and added to urls.py

--- CMSReScheduler/CMSReScheduler/views.py
+++ CMSReScheduler/CMSReScheduler/views.py
@@ -11,6 +11,29 @@ from forms import UploadCsv
 def home(request):
 	return render(request, 'test.html')
 
+
+def import_csv_file(request, type):
+    if type == 'schedule':
+        form = UploadCsv(type, request.FILES)
+        format = ['code', 'name', 'enrolment']
+        parser_list = parse(request.FILES['file'], format, ',')
+        update_courses(parser_list)
+        return HttpResponse('The schedule file has been uploaded!')
+    elif type == 'room':
+        form = UploadCsv(type, request.FILES)
+        format = ['code', 'name', 'building']
+        parser_list = parse(request.FILES['file'], format, ',')
+        update_rooms(parser_list)
+        return HttpResponse('Room file has been uploaded!')
+    elif type == 'department':
+        form = UploadCsv(type, request.FILES)
+        format = ['code', 'name']
+        parser_list = parse(request.FILES['file'], format, ',')
+        update_departments(parser_list)
+        return HttpResponse('Department file has been uploaded!')
+    else:
+        return HttpResponse('Invalid Type!')
+
 def csvimport(request):
     if request.method == 'POST':
         form = UploadCsv(request.POST, request.FILES)
@@ -30,3 +53,9 @@ def admin(request):
 
 def admin_upload(request):
 	return render(request, 'admin/upload.html')
+
+def instructor_schedule(request, name):
+    return render_to_request('schedule.html',)
+
+
+
