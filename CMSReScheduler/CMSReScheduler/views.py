42ab5482e0fa9ec1f2934fa6cc0328d801643c58
William-Mak <william.mak@hotmail.ca>
2013-10-24 16:14:09 -0400

    adding some preliminary work on the view for courses, as well as fixing the CSVupload

--- CMSReScheduler/CMSReScheduler/views.py
+++ CMSReScheduler/CMSReScheduler/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import utils.csvutils as csvutils
 from django.shortcuts import render
 from classes.models import Course
@@ -7,26 +10,59 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
 from forms import UploadCsv
+import simplejson as json
 
 def home(request):
 	return render(request, 'test.html')
 
 def csvimport(request):
-    if request.method == 'POST':
-        form = UploadCsv(request.POST, request.FILES)
-        # The format is hardcoded and following the database's structure
-        # because we don't know how the csv file will be yet. 
-        # FIX that when we do.
-        format = ['code', 'name', 'enrolment']    
-        parsed = parse(request.FILES['file'], format, ',')
-        update_courses(parsed)
-        return HttpResponse('CSV file imported sucessfully.')
-    else:
-        form = UploadCsv()
-    return render_to_response('csvimport.html', {'form': form}, context_instance=RequestContext(request))
-    
+	if request.method == 'POST':
+		form = UploadCsv(request.POST, request.FILES)
+		# The format is hardcoded and following the database's structure
+		# because we don't know how the csv file will be yet. 
+		# FIX that when we do.
+		format = ['code', 'name', 'enrolment', 'department']
+		parsed = csvutils.parse(request.FILES['file'], format, ',')
+		csvutils.update_courses(parsed)
+		return HttpResponse('CSV file imported sucessfully.')
+	else:
+		form = UploadCsv()
+	return render_to_response('csvimport.html', {'form': form}, context_instance=RequestContext(request))
+	
 def admin(request):
 	return render(request, 'admin/index.html')
 
 def admin_upload(request):
 	return render(request, 'admin/upload.html')
+
+def course(request, course):
+	''' 
+		Perform an action on a course 
+		PUT -- Modify the course
+		GET -- All relevant information pertaining to this course
+		DELETE -- Remove this course from the schedule (It will still exist as a course)
+		POST -- Add a new course
+	'''
+	if request.method == "PUT":
+		pass
+	elif request.method == "GET":
+		entry = Course.objects.filter(code=course)
+		if entry.count() != 0:
+			try:
+				info = {"Name" : entry[0].name, "Enrolment" : entry[0].enrolment, "Department" : entry[0].department}
+				status = 200
+			except Exception as e:
+				info = {"Error" : "Internal error occured" + e}
+				status = 500
+		else:
+			info = {"Error" : "Unknown course code"}
+			status = 404
+	elif request.method == "DELETE":
+		pass
+	elif request.method == "POST":
+		pass
+	else:
+		info = {"Error" : "Unknown request"}
+		status = 400
+	data = json.dumps(info)
+	return HttpResponse(content = data, status = status)
