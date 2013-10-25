8e3c573658af2fcd7195269b4d370ef8f9d053f9
daniel-almeida <daniel@daniels-MacBook-Air.local>
2013-10-25 16:53:11 -0400

    Add userregistration template

--- CMSReScheduler/CMSReScheduler/settings.py
+++ CMSReScheduler/CMSReScheduler/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/daniel/Documents/PythonProjects/Lethargic-Development/CMSReScheduler/CMSReScheduler',                      # Or path to database file if using sqlite3.
+        'NAME': 'CMSReSchedulerDB',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',           # I'm sorry. Sorta. not really. Should change to a better password down the road.
