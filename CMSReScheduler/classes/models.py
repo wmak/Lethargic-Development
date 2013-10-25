3ab90d4652bd195fca48b260bcfe54ceb7dbded8
Ives Fernando M S de Moura <ivesfmoura@gmail.com>
2013-10-25 17:57:58 -0300

    Modified User models
    Modified Instructor model:
    - Renamed from Lecturer to Instructor
    - Added a many to many field to store the instructor's courses
    Modified Chair model:
    -It now inherits from Instructor
    - Added som methods for specific tasks that only Chairs can do
    Added UndergradAdminAssistant model:
    - Added methods specific for this role.

--- CMSReScheduler/classes/models.py
+++ CMSReScheduler/classes/models.py
@@ -35,16 +35,54 @@ class User(models.Model):
 	def __unicode__(self):
 		return self.name
 
-class Lecturer(User):			#incomplete
+class Instructor(User):			#incomplete
 	room = models.ForeignKey(Room)
-
-class Chair(User):				#incomplete
-	room = models.ForeignKey(Room)
-
-class Schedule(models.Model):
-	lecturer = models.ForeignKey(Lecturer)
-	course = models.ForeignKey(Course)
-	room = models.ForeignKey(Room)
+	myCourses = models.ManyToManyField(Course)
 
 	def __unicode__(self):
-		return u'%s\n%s\n%s' % (self.room.code, self.course.code, self.lecturer.name);
+		return u'Professor %s' % (self.name)
+
+class Chair(Instructor):				#incomplete
+	
+	def prohibitChanges():
+		#TODO
+
+	def viewDepartmentCourses():
+		return Course.objects.filter(department = self.department)
+
+	def viewDepartmentInstructors():
+		return Instructor.objects.filter(department = self.department)
+
+	def viewInstructorsSchedules():
+		instructors = viewDepartmentInstructors();
+		schedules = []
+		for i in instructors:
+			schedules.append(Schedule.objects.filter(instructor = i))
+		return schedules
+
+class UndergradAdminAssistant(User):
+
+	#This method returns all classrooms, that is,
+	#rooms with capacity different from 1.
+	def listClassrooms():
+		return Room.objects.get(~Q(capacity = 1))
+
+	def getChairs():
+		return Chair.objects.all
+
+	def getInstructorsOfDepartment(dept):
+		return Instructor.objects.filter(department = dept)
+
+	def checkEnrolment(courseCode):
+		c = Course.objects.get(code = courseCode)
+		return c.enrolment
+
+class Schedule(models.Model):
+	instructor = models.ForeignKey(Instructor)
+	course = models.ForeignKey(Course)
+	room = models.ForeignKey(Room)
+	startTime = models.TimeField()
+	endTime = models.TimeField();
+
+	def __unicode__(self):
+		return u'%s\n%s\n%s\n%s - %s' % (self.room.code, self.course.code, self.instructor.name, startTime, endTime);
