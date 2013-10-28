596081a7e81e31d026af2e4492421699bac797ea
Jeffrey Cheung <jeffrey.cheung1337@gmail.com>
2013-10-28 16:22:41 -0400

    Schedule layout

--- CMSReScheduler/CMSReScheduler/settings.py
+++ CMSReScheduler/CMSReScheduler/settings.py
@@ -120,7 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'classes'
+    'classes',
+    'CMSReScheduler',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
---
+++ CMSReScheduler/CMSReScheduler/templatetags/schedule.py
@@ -0,0 +1,41 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_range(value):
+	return range(value)
+
+@register.filter
+def schedule_cell_day(value):
+	if value == 1:
+		return "schedule-day"
+	else:
+		return ""
+
+@register.filter
+def schedule_day(value):
+	# TODO: Refactor
+	if value == 1:
+		return "Monday"
+	elif value == 2:
+		return "Tuesday"
+	elif value == 3:
+		return "Wednesday"
+	elif value == 4:
+		return "Thursday"
+	elif value == 5:
+		return "Friday"
+	else:
+		return ""
+
+@register.filter
+def schedule_cell_time(value):
+	if value == 1:
+		return "schedule-time"
+	else:
+		return ""
+
+@register.filter
+def schedule_time(value):
+	return str(value + 5) + ":00"
