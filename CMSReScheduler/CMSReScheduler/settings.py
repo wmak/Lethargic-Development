47cb9dd788a468b976f996985e45a6df474b8af0
daniel-almeida <danielalmeida.ti@gmail.com>
2013-11-04 13:01:19 -0500

    Removing comments @ settings.py
    Removing comments made by Mak when we're using PostgreSQL/MySQL

--- CMSReScheduler/CMSReScheduler/settings.py
+++ CMSReScheduler/CMSReScheduler/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': 'CMSdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
-        'PASSWORD': '',           # I'm sorry. Sorta. not really. Should change to a better password down the road.
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
