3662b4e7167de359ede19b35896e0d5848343317
ShaiMitchell <shaimitchell@Shai-Mitchells-MacBook.local>
2013-11-04 01:57:18 -0500

    added some test cases using ives new implementation of the models

--- CMSReScheduler/classes/models.py
+++ CMSReScheduler/classes/models.py
@@ -1,96 +1,130 @@
 from django.db import models
 
 # Create your models here.
+class DepartmentManager(models.Manager):
+    def create_department(self, name, numberOfLecturers):
+        department = self.create(name=name, numberOfLecturers=numberOfLecturers)
+        return department
 
 class Department(models.Model):
-	name = models.CharField(max_length=30)
-	numberOfLecturers = models.IntegerField()
+        name = models.CharField(max_length=30)
+        numberOfLecturers = models.IntegerField()
 
-	def __unicode__(self):
-		return self.name
+        def __unicode__(self):
+                return self.name
 
+class CourseManager(models.Manager):
+    def create_course(self, name, code, enrolment, department):
+        course = self.create(name=name, code=code, enrolment=enrolment, department=department)
+        return course
 
 class Course(models.Model):
-	''' We need to have a "day", "start_time", and "end_time"  field for the courses so that we can pass
-    them to the front end to use to actually move the schedule around I think. '''
-	code = models.CharField(max_length=9) #ex. CSCC01H3F
-	name = models.CharField(max_length=50)
-	enrolment = models.IntegerField()
-	department = models.ForeignKey(Department)
-	day = models.CharField(max_length=10)
-	startTime = models.TimeField()
-	endTime = models.TimeField()
 
-	def __unicode__(self):
-		return u'%s - %s' % (self.code, self.name)
+        code = models.CharField(max_length=9) #ex. CSCC01H3F
+        name = models.CharField(max_length=50)
+        enrolment = models.IntegerField()
+        department = models.ForeignKey(Department)
+
+        def __unicode__(self):
+                return u'%s - %s' % (self.code, self.name)
+
+class RoomManager(models.Manager):
+    def create_croom(self, code, capacity):
+        room = self.create(code=code, capacity=capacity)
+        return rooom
 
 class Room(models.Model):
-	code = models.CharField(max_length=10) #ex. IC220
-	capacity = models.IntegerField()
+        code = models.CharField(max_length=10) #ex. IC220
+        capacity = models.IntegerField()
 
-	def __unicode__(self):
-		return self.code
+        def __unicode__(self):
+                return self.code
+
+        def getSchedule():
+                return CourseSchedule.objects.filter(room = self)
+
+
+class UserManager(models.Manager):
+    def create_user(self, name, address, email, department):
+        user = self.create(name=name, address=address, email=email, department=department)
+        return user
 
 class User(models.Model):
-	name = models.CharField(max_length=30)
-	address = models.CharField(max_length=50)
-	email = models.EmailField()
-	department = models.ForeignKey(Department)
+        name = models.CharField(max_length=30)
+        address = models.CharField(max_length=50)
+        email = models.EmailField()
+        department = models.ForeignKey(Department)
 
-	def __unicode__(self):
-		return self.name
+        def __unicode__(self):
+                return self.name
 
-class Instructor(User):			#incomplete
-	room = models.ForeignKey(Room)
-	myCourses = models.ManyToManyField(Course)
 
-	def __unicode__(self):
-		return u'Professor %s' % (self.name)
+class Instructor(User):                        #incomplete
+        room = models.ForeignKey(Room)
+        myCourses = models.ManyToManyField(Course)
 
-class Chair(Instructor):				#incomplete
-	
-	def prohibitChanges():
-		pass #TODO
+        def __unicode__(self):
+                return u'Professor %s' % (self.name)
 
-	def viewDepartmentCourses():
-		return Course.objects.filter(department = self.department)
+        def getSchedule():
+                schedule = []
+                for c in myCourses:
+                        schedule.append(CourseSchedule.objects.filter(course = c))
+                return schedule
 
-	def viewDepartmentInstructors():
-		return Instructor.objects.filter(department = self.department)
+class Chair(Instructor):                                #incomplete
+        
+        def prohibitChanges():
+                #TODO
+                return
 
-	def viewInstructorsSchedules():
-		instructors = viewDepartmentInstructors();
-		schedules = []
-		for i in instructors:
-			schedules.append(Schedule.objects.filter(instructor = i))
-		return schedules
+        def viewDepartmentCourses():
+                return Course.objects.filter(department = self.department)
+
+class Chair(Instructor):                                #incomplete
+        
+        def prohibitChanges():
+                #TODO
+
+        def viewDepartmentInstructors():
+                return Instructor.objects.filter(department = self.department)
+
+        def viewInstructorsSchedules():
+                instructors = viewDepartmentInstructors();
+                schedules = []
+                for i in instructors:
+                        schedules.append(i.getSchedule())
+                return schedules
+
+
 
 class UndergradAdminAssistant(User):
 
-	#This method returns all classrooms, that is,
-	#rooms with capacity different from 1.
-	def listClassrooms():
-		return Room.objects.get(~Q(capacity = 1))
+        #This method returns all classrooms, that is,
+        #rooms with capacity different from 1.
+        def listClassrooms():
+                return Room.objects.get(~Q(capacity = 1))
 
-	def getChairs():
-		return Chair.objects.all
+        def getChairs():
+                return Chair.objects.all
 
-	def getInstructorsOfDepartment(dept):
-		return Instructor.objects.filter(department = dept)
+        def getInstructorsOfDepartment(dept):
+                return Instructor.objects.filter(department = dept)
 
-	def checkEnrolment(courseCode):
-		c = Course.objects.get(code = courseCode)
-		return c.enrolment
+        def checkEnrolment(courseCode):
+                c = Course.objects.get(code = courseCode)
+                return c.enrolment
 
-class Schedule(models.Model):
-	''' I don't understand how this one works. A schedule should have all the courses an instructor
-    has not just one I think. Also, it should have the day and their start and end times no? I think this would be easier to 
-    implement in the courses model. '''
-	instructor = models.ForeignKey(Instructor)
-	course = models.ForeignKey(Course)
-	room = models.ForeignKey(Room)
-	# startTime = models.TimeField()
-	# endTime = models.TimeField()
+class CourseScheduleManager(models.Manager):
+    def create_course_schedule(self, course, room, dayOfWeek, department, length, typeOfSession):
+        course_schedule = self.create(course=course, room=room, dayOfWeek=dayOfWeek, startTime=startTime, \
+        	length=length, typeOfSession=typeOfSession)
+        return course_schedule
 
-	def __unicode__(self):
-		return u'%s\n%s\n%s\n%s - %s' % (self.room.code, self.course.code, self.instructor.name);
+class CourseSchedule(models.Model):
+        course = models.ForeignKey(Course)
+        room = models.ForeignKey(Room)
+        dayOfWeek = models.CharField(max_length = 9)
+        startTime = models.TimeField()
+        length = models.IntegerField() #in minutes
+        typeOfSession = models.CharField(max_length = 3) # LEC, TUT or PRA
--- CMSReScheduler/classes/tests.py
+++ CMSReScheduler/classes/tests.py
@@ -6,11 +6,70 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models.py import *
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+# Model Testing 
+
+class DepartmentTest(TestCase):
+	def setUp(self):
+		Department.objects.create_department('Computer Science', 13)
+
+	def test_setUp(self):
+		comp_sci = Department.objects.get(name="Computer Science")
+		self.assertEqual(comp_sci.name, 'Computer Science')
+		self.assertEqual(comp_sci.numberOfLecturers, 13)
+
+	def test_string(self):
+		comp_sci = Department.objects.get(name="Computer Science")
+		self.assertEqual(comp_sci, 'Computer Science')
+
+
+class CourseTest(TestCase):
+	def setUp(self):
+		Course.objects.create_course('Introduction to Software Engineering', 'cscc01h3', 200, 'Coputter Science')
+
+	def test_setUp(self):
+		course = Course.objects.get(code="cscc01h3")
+		self.assertEqual(course.code, 'cscc01h3')
+		self.assertEqual(course.name, 'Introduction to Software Engineering')
+		self.assertEqual(course.enrolment, 200)
+		self.assertEqual(course.department, 'Computer Science')
+
+	def test_string(self):
+		course = Course.objects.get(code="cscc01h3")
+		self.assertEqual(course, 'cscc01h3 - Introduction to Software Engineering')
+
+
+class RoomTest(TestCase):
+	def setUp(self):
+		Room.objects.create_course('IC220', 150)
+
+	def test_setUp(self):
+		room = Room.objects.get(code="IC220")
+		self.assertEqual(room.code, 'IC220')
+		self.assertEqual(room.capacity, 150)
+
+	def test_string(self):
+		room = Room.objects.get(code="IC220")
+		self.assertEqual(room, 'IC220')
+
+
+class UserTest(TestCase):
+	def setUp(self):
+		User.objects.create_course('Shai Mitchell', '1295 Military Trail, Scarborough, Ontarro', \
+			'shai.mitchell@email.ca', 'Coputter Science')
+
+	def test_setUp(self):
+		user = Course.objects.get(name="Shai Mitchell")
+		self.assertEqual(user.name, 'Shai Mitchell')
+		self.assertEqual(user.address, '1295 Military Trail, Scarborough, Ontarro')
+		self.assertEqual(user.email, 'shai.mitchell@email.ca')
+		self.assertEqual(user.department, 'Computer Science')
+		
+	def test_string(self):
+		user = Course.objects.get(name="Shai Mitchell")
+		self.assertEqual(user, 'Shai Mitchell')
+
+
+# End Of Model Testing
