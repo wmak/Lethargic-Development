34d81b36371e2f86db130758c12aa43acd8a1d77
William-Mak <william.mak@hotmail.ca>
2013-11-16 16:48:35 -0500

    adding Upload page module and some small testscripts

--- automation/modules/pages/admin/schedule.py
+++ automation/modules/pages/admin/schedule.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from saunter.po.webdriver.page import Page
 from saunter.ConfigWrapper import ConfigWrapper as cfg_wrapper
+from navigation import AdminNavigationPage
+from selenium.webdriver.support.wait import WebDriverWait
 
 locators = {
+	"schedule container" : "css=.content.fleft",
+	"time slot" : "xpath=//div[contains(@class, 'content')]/div[XVALUE]/div[YVALUE]",
 }
 
-class AdminSchedulePage(Page):
+class AdminSchedulePage(AdminNavigationPage):
 	def __init__(self, driver):
 		self.driver = driver
 		self.config = cfg_wrapper().config
@@ -13,3 +17,14 @@ class AdminSchedulePage(Page):
 	def open(self):
 		self.driver.get("%s/admin/" % self.config.get("Selenium", "base_url"))
 		return self	
+
+	def validate(self):
+		w = WebDriverWait(self.driver, 30)
+		w.until(lambda driver: self.driver.is_visible(locators['schedule container']))
+		return self
+
+	def schedule(self, x, y):
+		x = str(x)
+		y = str(y)
+		locator = locators["time slot"].replace("XVALUE", x).replace("YVALUE", y)
+		return self.driver.find_element_by_locator(locator).text
--- automation/scripts/admin_schedule.py
+++ automation/scripts/admin_schedule.py
@@ -7,5 +7,26 @@ import pytest
 
 class CheckAdminSchedulePage(SaunterTestCase):    
 	@pytest.marks('regression')
-	def test_valid_random_cityname(self):
-		admin = AdminSchedulePage(self.driver).open()
+	def test_schedule_has_Monday(self):
+		admin = AdminSchedulePage(self.driver).open().validate()
+		self.matchers.verify_equal(admin.schedule(2,1), "Monday")
+
+	@pytest.marks('regression')
+	def test_schedule_has_Tuesday(self):
+		admin = AdminSchedulePage(self.driver).open().validate()
+		self.matchers.verify_equal(admin.schedule(3,1), "Tuesday")
+
+	@pytest.marks('regression')
+	def test_schedule_has_Wednesday(self):
+		admin = AdminSchedulePage(self.driver).open().validate()
+		self.matchers.verify_equal(admin.schedule(4,1), "Wednesday")
+
+	@pytest.marks('regression')
+	def test_schedule_has_Thursday(self):
+		admin = AdminSchedulePage(self.driver).open().validate()
+		self.matchers.verify_equal(admin.schedule(5,1), "Thursday")
+
+	@pytest.marks('regression')
+	def test_schedule_has_Friday(self):
+		admin = AdminSchedulePage(self.driver).open().validate()
+		self.matchers.verify_equal(admin.schedule(6,1), "Friday")
